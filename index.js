5a294365acd2b1b7e939ea7084aaa38e81f147f1
Kendell Joseph <kendell@mallowfields.com>
2019-07-25 16:22:25 -0400

    Adds index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+// # GRJS
+// [Github](https://github.com/grandrapidsjavascript/lights-cameras-actions) / [Meetup](https://www.meetup.com/Grand-Rapids-JavaScript-Meetup/) / [Stories(Pivotal Tracker)](https://www.pivotaltracker.com/n/projects/2129675)
+// 
+// > ### Lights: [⚙️](/docs/features/step_definitions/lights.html)
+// > [/index.js](/docs/lights/index.html)
+// >
+// > ### Cameras: [⚙️](/docs/features/step_definitions/cameras.html)
+// > [/index.js](/docs/cameras/index.html)
+// >
+// > ### Actions: [⚙️](/docs/features/step_definitions/actions.html)
+// > [/index.js](/docs/actions/index.html)
+// >
+// > ### (Development): [⚙️](/docs/features/step_definitions/utilities.html)
+// > [/gulpfile.js](/docs/gulpfile.html)
+// ---
+// Notes:
+
+require('colors');
+
+
+// ---
+// 2019 By [Mallowfields](https://mallowfields.com)
