97d8d9c48619941f15b1a17d87b02c749b9d2149
Kendell Joseph <kendell@mallowfields.com>
2019-07-25 16:23:05 -0400

    Adds cameras.js

---
+++ cameras/index.js
@@ -0,0 +1,10 @@
+// # [GRJS...](/docs/)index.js
+// ## JavaScript Cameras
+//
+// JavaScript that makes cameras do things and behave certain ways.
+//
+// #### Dependency Documentation
+// * OpenCV - [tutorial](https://community.risingstack.com/opencv-tutorial-computer-vision-with-node-js/) / [documentation](https://github.com/peterbraden/node-opencv) / [package](https://www.npmjs.com/package/opencv)
+// 
+// ---
+// Notes:
