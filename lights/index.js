c3885163352010491d36e9d88e922786801a4f05
Kendell Joseph <kendell@mallowfields.com>
2019-07-25 16:22:46 -0400

    Adds lights.js

---
+++ lights/index.js
@@ -0,0 +1,10 @@
+// # [GRJS...](/docs/)index.js
+// ## JavaScript Lights
+//
+// JavaScript that makes light do things and behave certain ways.
+//
+// #### Dependency Documentation
+// * OpenCV - [tutorial](https://community.risingstack.com/opencv-tutorial-computer-vision-with-node-js/) / [documentation](https://github.com/peterbraden/node-opencv) / [package](https://www.npmjs.com/package/opencv)
+// 
+// ---
+// Notes:
