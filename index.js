eda1ade33bba6468ff05eefbe445836231268982
Kendell Joseph <kendell@mallowfields.com>
2019-07-25 16:38:26 -0400

    Debugging docs.. WIP

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // # GRJS
 // [Github](https://github.com/grandrapidsjavascript/lights-cameras-actions) / [Meetup](https://www.meetup.com/Grand-Rapids-JavaScript-Meetup/) / [Stories(Pivotal Tracker)](https://www.pivotaltracker.com/n/projects/2129675)
 // 
-// > ### Lights: [⚙️](/docs/features/step_definitions/lights.html)
-// > [/index.js](/docs/lights/index.html)
+// > ### Lights: [⚙️](features/step_definitions/lights.html)
+// > [/index.js](lights/index.html)
 // >
-// > ### Cameras: [⚙️](/docs/features/step_definitions/cameras.html)
-// > [/index.js](/docs/cameras/index.html)
+// > ### Cameras: [⚙️](features/step_definitions/cameras.html)
+// > [/index.js](cameras/index.html)
 // >
-// > ### Actions: [⚙️](/docs/features/step_definitions/actions.html)
-// > [/index.js](/docs/actions/index.html)
+// > ### Actions: [⚙️](features/step_definitions/actions.html)
+// > [/index.js](actions/index.html)
 // >
-// > ### (Development): [⚙️](/docs/features/step_definitions/utilities.html)
-// > [/gulpfile.js](/docs/gulpfile.html)
+// > ### (Development): [⚙️](features/step_definitions/utilities.html)
+// > [/gulpfile.js](gulpfile.html)
 // ---
 // Notes:
 
