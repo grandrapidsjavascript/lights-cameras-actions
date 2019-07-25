e6de2978b2906c5e277244f7b52311625f301d2f
Kendell Joseph <kendell@mallowfields.com>
2019-07-25 16:23:21 -0400

    Adds actions

---
+++ actions/index.js
@@ -0,0 +1,10 @@
+// # [GRJS...](/docs/)index.js
+// ## JavaScript Actions
+//
+// JavaScript that makes things behave certain ways.
+//
+// #### Dependency Documentation
+// * Johnny-Five - [documentation](http://johnny-five.io/) / [package](https://www.npmjs.com/package/johnny-five)
+// 
+// ---
+// Notes:
